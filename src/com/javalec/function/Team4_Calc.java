ebc7a07d7d3ecefe2efe7493c6fe2b46c6dcffcf
Rano-K <minsoo0704@naver.com>
2023-04-18 17:31:39 +0900

    main생성
    -김민수
    -main코드 작성
    입력부분만 작성

---
+++ src/com/javalec/function/Team4_Calc.java
@@ -0,0 +1,25 @@
+package com.javalec.function;
+
+import java.util.Scanner;
+
+public class Team4_Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		int firstNum=0, secondNum=0; //첫번째 숫자, 두번째 숫자 초기화
+		int add, sub, mul; //덧셈, 뺄셈, 곱셈 값
+		double divide, reminder1, reminder2; //나눗셈, 몫, 나머지 값
+		
+		//입력부분
+		System.out.print("첫번째 숫자를 입력하세요: ");
+		firstNum = scanner.nextInt();
+		System.out.print("두번째 숫자를 입력하세요: ");
+		secondNum = scanner.nextInt();
+		
+		//출력부분
+		
+	}
+
+}
